3f50292671d5104381ea6ff0adc22720611ad758
U-PC\Owner <s14t224@stmail.eng.kagawa-u.ac.jp>
2015-10-28 13:19:10 +0900

    定数を宣言しているクラス

--- Common.java
+++ Common.java
@@ -2,10 +2,10 @@ package RPG;
 
 public interface Common {
 	// 方向を表す定数
-	public static final int LEFT = 11;
-	public static final int RIGHT = 9;
-	public static final int UP = 8;
-	public static final int DOWN = 10;
+	public static final int LEFT = 3;
+	public static final int RIGHT = 1;
+	public static final int UP = 0;
+	public static final int DOWN = 2;
 
     // チップセットのサイズ
     public static final int CS = 32;
